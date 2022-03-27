0cede5e44163527d87c28ebb88eb1889695bb63d
SunHarvey <sunharvey19@gmail.com>
2022-03-27 20:05:07 +0800

    add map

---
+++ chapter7-map/213.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	ranks := make(map[string]int)
+	ranks["gold"] = 1
+	ranks["silver"] = 2
+	ranks["bronze"] = 3
+	fmt.Println(ranks["bronze"])
+	fmt.Println(ranks["gold"])
+
+	elements := make(map[string]string)
+	elements["H"] = "Hydrogen"
+	elements["Li"] = "Lithium"
+	fmt.Println(elements["Li"])
+	fmt.Println(elements["H"])
+
+	isPrime := make(map[int]bool)
+	isPrime[4] = false
+	isPrime[7] = true
+	fmt.Println(isPrime[4])
+	fmt.Println(isPrime[7])
+}
