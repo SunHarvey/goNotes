b11c5cafabc0f2850842c237a4487dde0667a2d3
SunHarvey <sunharvey19@gmail.com>
2022-03-31 19:50:53 +0800

    add chapter8 struct

---
+++ chapter8-struct/232.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	subscriber := []string{}
+	subscriber = append(subscriber, "Aman Singh")
+	subscriber = append(subscriber, 4.99)
+	subscriber = append(subscriber, true)
+}
---
+++ chapter8-struct/232_2.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	subscriber := map[string]float64{}
+	subscriber["name"] = "Aman Singh"
+	subscriber["rate"] = 4.99
+	subscriber["active"] = true
+}
