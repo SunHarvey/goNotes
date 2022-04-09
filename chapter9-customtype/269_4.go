45abd58fd09454810f56a376634a2514c431e3f2
SunHarvey <sunharvey19@gmail.com>
2022-04-09 19:42:16 +0800

    Define types and operators

---
+++ chapter9-customtype/269_4.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Liters float64
+type Gallons float64
+
+func main() {
+	fmt.Println(Liters(1.2) + 3.4)
+	fmt.Println(Gallons(5.5) - 2.2)
+	fmt.Println(Gallons(1.2) == 1.2)
+	fmt.Println(Liters(1.2) < 3.4)
+
+	fmt.Println(Liters(1.2) + Gallons(3.4))
+	fmt.Println(Gallons(1.2) == Liters(1.2))
+}
