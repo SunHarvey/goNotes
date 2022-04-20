70a9ad50a0a1e2121b987c4615e74e65f8d90efb
SunHarvey <sunharvey19@gmail.com>
2022-04-20 08:57:39 +0800

    move date type to gopath

---
+++ chapter10-Packaging-embedding/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"github.com/headfirstgo/calendar"
+)
+
+func main() {
+	date := calendar.Date{}
+	date.Year = 2022
+	date.Month = 14
+	date.Day = 50
+	fmt.Println(date)
+	date = calendar.Date{Year: 0, Month: 0, Day: -2}
+	fmt.Println(date)
+
+}
