c1183399da3e791cd300bb9ec518ead23d53df45
SunHarvey <sunharvey19@gmail.com>
2022-04-23 18:57:34 +0800

    add export function

---
+++ chapter10-Packaging-embedding/309.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/headfirstgo/calendar"
+
+func main() {
+	event := calendar.Event{}
+	event.month = 5
+}
---
+++ chapter10-Packaging-embedding/309_2.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/headfirstgo/calendar"
+
+func main() {
+	event := calendar.Event{}
+	event.Date.year = 2022
+}
