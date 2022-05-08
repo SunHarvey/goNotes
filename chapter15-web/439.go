e5d2c1f206b50149189112a5b2f8505e052e2100
SunHarvey <sunharvey19@gmail.com>
2022-05-08 18:22:10 +0800

    end chapter15-web

---
+++ chapter15-web/439.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func callFunction(passedFunction func()) {
+	passedFunction()
+}
+
+func callTwich(passedFunction func()) {
+	passedFunction()
+	passedFunction()
+}
+
+func callWithArguments(passedFunction func(string, bool)) {
+	passedFunction("This sentense is", false)
+}
+
+func printReturnValue(passedFunction func() string) {
+	fmt.Println(passedFunction())
+}
+
+func functionA() {
+	fmt.Println("function called")
+}
+
+func functionB() string {
+	fmt.Println("funciton called")
+	return "Returning from function"
+}
+
+func functionC(a string, b bool) {
+	fmt.Println("function called")
+	fmt.Println(a, b)
+}
+
+func main() {
+	callFunction(functionA)
+	callTwich(functionA)
+	callWithArguments(functionC)
+	printReturnValue(functionB)
+}
