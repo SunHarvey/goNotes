68443908449b3aa3a87dc119b82adb8af0f6f80a
SunHarvey <sunharvey19@gmail.com>
2022-05-20 07:58:36 +0800

    add  DataPersistence

---
+++ BlockchainDevelopment/ApplicationDevelopment/DataPersistence/daemon_boltDB.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/boltdb/bolt"
+	"fmt"
+)
+
+func main() {
+	db,_ := bolt.Open("my.db", 0600, nil)
+	defer db.Close()
+	db.Update(func(tx *bolt.Tx) error {
+		bucket,_ := tx.CreateBucket([]byte("bucket1"))
+		bucket.Put([]byte("name"), []byte("yekai"))
+		return nil
+	})
+
+	db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("bucket1"))
+		val:= bucket.Get([]byte("name"))
+		fmt.Println(string(val))
+		return nil
+	})
+}
