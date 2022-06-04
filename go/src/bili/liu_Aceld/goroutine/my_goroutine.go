b20888c71938bf7577f7a41f15ad6a8525a4e40d
SunHarvey <sunharvey19@gmail.com>
2022-06-04 11:00:04 +0800

    update my goroutine

--- go/src/bili/liu_Aceld/goroutine/my_goroutine.go
+++ go/src/bili/liu_Aceld/goroutine/my_goroutine.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-    "fmt"
-    "time"
+	"fmt"
+	"time"
 )
 
 func hello_world(num int) {
-    fmt.Println("hello world", num)
+	fmt.Println("hello world", num)
 }
+
 func main() {
-    for i:=0; i<5; i++ {
-        go hello_world(i)
-    }
-    time.Sleep(1 * time.Second)
+	for i := 0; i < 5; i++ {
+		go hello_world(i)
+	}
+	time.Sleep(1 * time.Second)
 }
-    
-    
