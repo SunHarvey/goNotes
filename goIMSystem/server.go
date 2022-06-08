6f521308c1ef1d018bd012ab48dbec15bb678855
SunHarvey <sunharvey19@gmail.com>
2022-06-08 18:35:07 +0800

    im system v2

--- goIMSystem/server.go
+++ goIMSystem/server.go
@@ -3,24 +3,63 @@ package main
 import (
     "fmt"
     "net"
+    "sync"
 )
 
 type Server struct {
     Ip string
     Port int
+    
+    //online user list
+    OnlineMap map[string]*User
+    mapLock sync.RWMutex
+
+    // message boardcast channel
+    Message chan string 
 }
 
 func NewServer(ip string, port int) *Server {
     server := &Server{
         Ip: ip,
         Port: port,
+        OnlineMap: make(map[string]*User),
+        Message: make(chan string),
     }
     return server
 }
 
+func (s *Server) ListenMessager() {
+    for {
+        msg := <- s.Message
+        s.mapLock.Lock()
+        for _, cli := range s.OnlineMap {
+            cli.C <- msg
+        }
+        s.mapLock.Unlock()
+    }
+}
+
+func (s *Server) BroadCast(user *User,msg string) {
+    sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
+    
+    s.Message <- sendMsg 
+}
+
 
 func (s *Server) Handler(conn net.Conn) {
-    fmt.Println("connect build success")
+    //fmt.Println("connect build success")
+    user := NewUser(conn)
+    
+    // user online ,add user to onlineMap
+    s.mapLock.Lock()
+    s.OnlineMap[user.Name] = user
+    s.mapLock.Unlock()
+    
+    // broadcast user online message
+    s.BroadCast(user, "I am online") 
+    
+    //
+    select {}
     
 }
 
@@ -34,6 +73,10 @@ func (s *Server) Start() {
     }
     // close listen socket
     defer listener.Close()
+
+
+    // start  listener message goroutine
+    go s.ListenMessager()
 
     for {
         // accept
