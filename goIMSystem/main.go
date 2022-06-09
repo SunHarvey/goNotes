8dd29557efd1823b9ea9097fa71802db44a68584
SunHarvey <sunharvey19@gmail.com>
2022-06-09 08:42:14 +0800

    update v4

--- goIMSystem/main.go
+++ goIMSystem/main.go
@@ -1,7 +1,6 @@
 package main
 
-
 func main() {
-    server := NewServer("127.0.0.1",8888)
-    server.Start()
+	server := NewServer("127.0.0.1", 8888)
+	server.Start()
 }
--- goIMSystem/server.go
+++ goIMSystem/server.go
@@ -1,98 +1,115 @@
 package main
 
 import (
-    "fmt"
-    "net"
-    "sync"
+	"fmt"
+	"io"
+	"net"
+	"sync"
 )
 
 type Server struct {
-    Ip string
-    Port int
-    
-    //online user list
-    OnlineMap map[string]*User
-    mapLock sync.RWMutex
+	Ip   string
+	Port int
 
-    // message boardcast channel
-    Message chan string 
+	//online user list
+	OnlineMap map[string]*User
+	mapLock   sync.RWMutex
+
+	// message boardcast channel
+	Message chan string
 }
 
 func NewServer(ip string, port int) *Server {
-    server := &Server{
-        Ip: ip,
-        Port: port,
-        OnlineMap: make(map[string]*User),
-        Message: make(chan string),
-    }
-    return server
+	server := &Server{
+		Ip:        ip,
+		Port:      port,
+		OnlineMap: make(map[string]*User),
+		Message:   make(chan string),
+	}
+	return server
 }
 
 func (s *Server) ListenMessager() {
-    for {
-        msg := <- s.Message
-        s.mapLock.Lock()
-        for _, cli := range s.OnlineMap {
-            cli.C <- msg
-        }
-        s.mapLock.Unlock()
-    }
+	for {
+		msg := <-s.Message
+		s.mapLock.Lock()
+		for _, cli := range s.OnlineMap {
+			cli.C <- msg
+		}
+		s.mapLock.Unlock()
+	}
 }
 
-func (s *Server) BroadCast(user *User,msg string) {
-    sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
-    
-    s.Message <- sendMsg 
-}
+func (s *Server) BroadCast(user *User, msg string) {
+	sendMsg := "[" + user.Addr + "]" + user.Name + ":" + msg
 
+	s.Message <- sendMsg
+}
 
 func (s *Server) Handler(conn net.Conn) {
-    //fmt.Println("connect build success")
-    user := NewUser(conn)
-    
-    // user online ,add user to onlineMap
-    s.mapLock.Lock()
-    s.OnlineMap[user.Name] = user
-    s.mapLock.Unlock()
-    
-    // broadcast user online message
-    s.BroadCast(user, "I am online") 
-    
-    //
-    select {}
-    
-}
+	//fmt.Println("connect build success")
+	user := NewUser(conn)
 
+	// user online ,add user to onlineMap
+	s.mapLock.Lock()
+	s.OnlineMap[user.Name] = user
+	s.mapLock.Unlock()
+
+	// broadcast user online message
+	s.BroadCast(user, "I am online")
+
+	// accept client message
+	go func() {
+		buf := make([]byte, 4096)
+		for {
+			n, err := conn.Read(buf)
+			if n == 0 {
+				s.BroadCast(user, "off line")
+				return
+			}
+
+			if err != nil && err != io.EOF {
+				fmt.Println("Conn Read err:", err)
+				return
+			}
+
+			// get user message, delete '\n'
+			msg := string(buf[:n-1])
+
+			// broadcast message
+			s.BroadCast(user, msg)
+		}
+	}()
+
+	//
+	select {}
+
+}
 
 func (s *Server) Start() {
-    // socket listen
-    listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
-    if err !=nil {
-        fmt.Println("net.Listen err:", err)
-        return
-    }
-    // close listen socket
-    defer listener.Close()
+	// socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", s.Ip, s.Port))
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	// close listen socket
+	defer listener.Close()
 
+	// start  listener message goroutine
+	go s.ListenMessager()
 
-    // start  listener message goroutine
-    go s.ListenMessager()
+	for {
+		// accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error:", err)
+			continue
+		}
 
-    for {
-        // accept
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Println("listener accept error:", err)
-            continue
-        }
-    
-        //do handler
-                
-        go s.Handler(conn)
+		//do handler
 
-        
-    }
+		go s.Handler(conn)
+
+	}
 }
-
-
-
