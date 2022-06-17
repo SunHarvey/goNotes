5ed269f182f1413cbaea59689a79a28e9ce0ebcc
SunHarvey <sunharvey19@gmail.com>
2022-06-17 19:35:16 +0800

    add goroutine

---
+++ MicroservicesInAction/chapter5/61_goroutine.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go func() {
+		for i := 0; i < 20; i++ {
+			fmt.Print(" ", i)
+		}
+	}()
+
+	fmt.Println()
+	for i := 0; i < 10; i++ {
+		fmt.Print(" ", i)
+	}
+	time.Sleep(2 * time.Second)
+}
