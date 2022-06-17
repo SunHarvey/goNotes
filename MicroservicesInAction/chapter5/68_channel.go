54d3f0d06bc1def20d411d4db8ef2e73dc0e51cd
SunHarvey <sunharvey19@gmail.com>
2022-06-17 20:41:08 +0800

    update

--- MicroservicesInAction/chapter5/68_channel.go
+++ MicroservicesInAction/chapter5/68_channel.go
@@ -1,44 +1,41 @@
 package main
 
 import (
-    "fmt"
-    "sync"
+	"fmt"
+	"sync"
 )
 
 const (
-    noGroutine = 5
-    noTask = 10
+	noGroutine = 5
+	noTask     = 10
 )
 
 var wg sync.WaitGroup
 
 func main() {
-    //
-    tasks := make(chan int, noTask)
-    
-    //  
-    for no := 1; no < noGroutine; no++ {
-        wg.Add(1)
-        go taskProcess(tasks, no)
-    }
-    
-    //
-    for taskNO :=1; taskNO <= noTask; taskNO++ {
-        tasks <- taskNO
-    }
-    close(tasks)
-    wg.Wait()
+	//
+	tasks := make(chan int, noTask)
+
+	//
+	for no := 1; no < noGroutine; no++ {
+		wg.Add(1)
+		go taskProcess(tasks, no)
+	}
+
+	//
+	for taskNO := 1; taskNO <= noTask; taskNO++ {
+		tasks <- taskNO
+	}
+	close(tasks)
+	wg.Wait()
 
 }
 
 func taskProcess(tasks chan int, workerNo int) {
-    defer wg.Done()
-    
-    for t:= range tasks {
-        fmt.Printf("Worker %d is processing Task no:%d \n", workerNo,t)
-    }
-    fmt.Printf("Worker %d got off work \n", workerNo)
+	defer wg.Done()
+
+	for t := range tasks {
+		fmt.Printf("Worker %d is processing Task no:%d \n", workerNo, t)
+	}
+	fmt.Printf("Worker %d got off work \n", workerNo)
 }
-
-
-
