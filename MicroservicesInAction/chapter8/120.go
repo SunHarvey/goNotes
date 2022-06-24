2fca7149a243f545e36f6f8dbce7ed2681f97670
SunHarvey <sunharvey19@gmail.com>
2022-06-24 08:49:59 +0800

    update sync

--- MicroservicesInAction/chapter8/120.go
+++ MicroservicesInAction/chapter8/120.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-//	"time"
+	//	"time"
 )
 
 func main() {
 	fmt.Println(getNumber())
 }
 
-
 func getNumber() int {
 	var i int
 	go func() {
 		i = 5
 	}()
-//	time.Sleep(1 * time.Second)
+	//	time.Sleep(1 * time.Second)
 	return i
 }
