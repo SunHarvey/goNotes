7454c84baab4e762050d0efe7b8b7a13182cdb8b
SunHarvey <sunharvey19@gmail.com>
2022-07-23 10:10:51 +0800

    update

--- Exercise/sum_list2.go
+++ Exercise/sum_list2.go
@@ -4,7 +4,7 @@ package main
 import "fmt"
 
 func main() {
-	list_a :=[7]int{1,3,5,7,9,11,13}
+	list_a :=[8]int{1,3,5,7,9,11,13,15}
 	sum := 0
 	for _,value := range list_a {
 		//fmt.Println(value)
